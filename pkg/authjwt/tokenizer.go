8caffd6a8257c82f2da16e1b3f6d4560cbfca671
Dr Weltschmerz <23312554+DrWeltschmerz@users.noreply.github.com>
2025-07-28 00:24:52 +0200

    refactor to more generic way, removed gin from usage, it will be moved to gin package for users

---
+++ pkg/authjwt/tokenizer.go
@@ -0,0 +1,66 @@
+package authjwt
+
+import (
+	"errors"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type JWTTokenizer struct {
+	secret string
+}
+
+func NewJWTTokenizer() *JWTTokenizer {
+	secret := os.Getenv("JWT_SECRET")
+	if secret == "" {
+		panic("JWT_SECRET environment variable is not set")
+	}
+	return &JWTTokenizer{secret: secret}
+}
+
+func (j *JWTTokenizer) GenerateToken(email, userID string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email":  email,
+		"userID": userID,
+		"exp":    time.Now().Add(2 * time.Hour).Unix(),
+	})
+	return token.SignedString([]byte(j.secret))
+}
+
+func (j *JWTTokenizer) VerifyToken(token string) (string, error) {
+	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+		return []byte(j.secret), nil
+	})
+	if err != nil || !parsedToken.Valid {
+		return "", errors.New("invalid or expired token")
+	}
+
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	if !ok {
+		return "", errors.New("invalid claims")
+	}
+
+	uid, ok := claims["userID"].(string)
+	if !ok {
+		return "", errors.New("userID claim is missing or invalid")
+	}
+
+	return uid, nil
+}
+
+func (j *JWTTokenizer) ExtractUserIDFromHeader(h http.Header) (string, error) {
+	auth := h.Get("Authorization")
+	const prefix = "Bearer "
+	auth = strings.TrimPrefix(auth, prefix)
+	if auth == "" {
+		return "", errors.New("no token provided")
+	}
+	return j.VerifyToken(auth)
+}
--- pkg/authjwt/types.go
+++
@@ -1,7 +0,0 @@
-package authjwt
-
-// ErrorMessage defines the structure for JSON error responses.
-type ErrorMessage struct {
-	ErrorCode int    `json:"errorCode"`
-	Message   string `json:"message"`
-}
